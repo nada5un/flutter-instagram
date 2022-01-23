ecc222fc5abdf6182ffe550271d53773e9933612
daeun <sung980524@naver.com>
2022-01-23 13:31:21 +0900

    아래로 스크롤시 하단바 안보이기

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import "./style.dart" as style;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import "package:flutter/rendering.dart";
 
 void main() {
   runApp(
@@ -22,13 +23,32 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var tab = 0;
   var data = [];
+  //0:false , 1:true
+  int isScrollDown = 0;
 
   getData() async{
     var request = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
-    setState(() {
-      data = jsonDecode(request.body);
-    });
+    if (request.statusCode == 200){
+      setState(() {
+        data = jsonDecode(request.body);
+      });
+    }else{
+
+    }
   }
+
+  appendPost(Map value)=>setState((){
+    print(value);
+    data.add(value);
+  });
+
+  changeTab(int value)=>setState((){
+    tab = value;
+  });
+
+  changeBottomState(int value)=>setState((){
+    isScrollDown = value;
+  });
 
   @override
   void initState() {
@@ -52,59 +72,128 @@ class _MyAppState extends State<MyApp> {
       ),
       body: [
         Home(
-          data:data
-        ),Text("샵페이지")][tab],
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,   // <-- HERE
-        showUnselectedLabels: false, // <-- AND HERE
-        onTap: (i){
-          setState(() {
-            tab = i;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(label:"홈",icon: Icon(Icons.home_outlined)),
-          BottomNavigationBarItem(label: "샵",icon: Icon(Icons.shopping_bag_outlined))
-        ],
-      ),
+          data:data,
+          appendPost:appendPost,
+          changeBottomState:changeBottomState
+        ),Text("샵페이지")
+      ][tab],
+
+      bottomNavigationBar: Offstage(
+        offstage: isScrollDown==1,
+        child: Bottom(
+            tab:tab,
+            changeTab:changeTab
+        ),
+      )
     );
   }
 }
 
 //홈화면
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   Home({
     Key? key,
-    this.data
+    this.data,
+    required this.appendPost,
+    required this.changeBottomState
   }) : super(key: key);
 
   final data;
+  final Function(Map value) appendPost;
+  final Function(int value) changeBottomState;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  var scroll = ScrollController();
+
+  getScroll() async{
+    var request = await http.get(Uri.parse('https://codingapple1.github.io/app/more1.json'));
+    print(request.body);
+    widget.appendPost(jsonDecode(request.body));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    scroll.addListener(() {
+      //최대 스크롤 했을 경우
+      if (scroll.position.pixels == scroll.position.maxScrollExtent){
+        getScroll();
+      }
+      //아래로 스크롤
+      if(scroll.position.userScrollDirection == ScrollDirection.reverse){
+        widget.changeBottomState(1);
+      }else{
+        widget.changeBottomState(0);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    print(data);
-    return ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (c,i){
-          return Column(
-              children: [
-                Image.network(data[i]["image"]),
-                Container(
-                  constraints: BoxConstraints(maxWidth: 600),
-                  padding: EdgeInsets.all(20),
-                  width: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("좋아요 ${data[i]["likes"]}",style: TextStyle(fontWeight: FontWeight.bold)),
-                      Text(data[i]["user"]),
-                      Text(data[i]["content"])
-                    ],
-                  ),
-                )
-              ]
-          );
-        }
+    if (widget.data.isNotEmpty){
+      return ListView.builder(
+          itemCount: widget.data.length,
+          controller: scroll,
+          itemBuilder: (c,i){
+            return Column(
+                children: [
+                  Image.network(widget.data[i]["image"]),
+                  Container(
+                    constraints: BoxConstraints(maxWidth: 600),
+                    padding: EdgeInsets.all(20),
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("좋아요 ${widget.data[i]["likes"]}",style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text(widget.data[i]["user"]),
+                        Text(widget.data[i]["content"])
+                      ],
+                    ),
+                  )
+                ]
+            );
+          }
+      );
+    }else{
+      return CircularProgressIndicator();
+    }
+  }
+}
+
+//하단 바
+class Bottom extends StatefulWidget {
+  Bottom({
+    Key? key,
+    this.tab,
+    required this.changeTab,
+  }) : super(key: key);
+
+  final tab;
+  final Function(int value) changeTab;
+
+  @override
+  State<Bottom> createState() => _BottomState();
+}
+
+class _BottomState extends State<Bottom> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      showSelectedLabels: false,   // <-- HERE
+      showUnselectedLabels: false, // <-- AND HERE
+      onTap: (i){
+        widget.changeTab(i);
+      },
+      items: [
+        BottomNavigationBarItem(label:"홈",icon: Icon(Icons.home_outlined)),
+        BottomNavigationBarItem(label: "샵",icon: Icon(Icons.shopping_bag_outlined))
+      ],
     );
   }
 }
